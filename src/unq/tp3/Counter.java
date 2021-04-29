0d1c98e1b22f640955371c2b71f0d517b79b6437
Rodrigo <Rodrigo@DESKTOP-70LGJ70>
2021-04-28 22:12:05 -0300

    Counter Resuelto

---
+++ src/unq/tp3/Counter.java
@@ -0,0 +1,54 @@
+package unq.tp3;
+
+import java.util.ArrayList;
+
+public class Counter {
+	private ArrayList<Integer> lista;
+	
+	
+	public Counter() {
+		this.setLista(new ArrayList<Integer>());
+	}
+
+	public void setLista(ArrayList<Integer> lista) {
+		this.lista = lista;
+	}
+	
+	public ArrayList<Integer> getLista() {
+		return lista;
+	}
+	
+	public void agregarNumero(Integer numero) {
+		this.lista.add(numero);
+	}
+
+	public Integer cantidadDePares() {
+		int pares = 0;
+		for (int numero : lista) {
+				if (numero%2 == 0) {
+					pares = pares + 1;
+				}
+		} 
+		return pares;
+	}
+	
+	public int cantidadDeImpares() {
+		int impares = 0;
+        for (int numero : lista) {
+            if(!(numero%2==0)) {
+            	impares = impares + 1;
+            }
+        }
+        return impares;
+	}
+
+	public int cantidadDeMultiplosDe(Integer multiplicador) {
+		int multiplos = 0;
+		for (int numero : lista) {
+			if(numero% multiplicador==0) {
+				multiplos = multiplos + 1;
+			}
+		}
+		return multiplos;
+	}
+}
---
+++ test/unq/tp3/CounterTestCase.java
@@ -0,0 +1,56 @@
+package unq.tp3;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CounterTestCase {    
+    private Counter counter;    
+    
+    /**
+     * Crea un escenario de test básico, que consiste en un contador 
+     * con 10 enteros
+     * 
+     * @throws Exception
+     */
+    @BeforeEach
+    public void setUp() throws Exception {
+ 
+   	 //Se crea el contador
+   	 counter = new Counter();
+   	 
+   	 //Se agregan los numeros. Un solo par y nueve impares
+   	 counter.agregarNumero(1);
+   	 counter.agregarNumero(20);
+   	 counter.agregarNumero(3);
+   	 counter.agregarNumero(40);
+   	 counter.agregarNumero(25);
+    }
+ 
+    /**
+     * Verificar la cantidad de pares
+     */
+    @Test
+    public void testNumerosPares() { 
+    	int cantidadDeNumerosPares = counter.cantidadDePares();	
+    	assertEquals(cantidadDeNumerosPares, 2);
+    }
+    /**
+     * Verificar la cantidad de impares
+     */
+    @Test
+    public void testNumerosImpares() {  	
+    	int cantidadDeNumerosImpares = counter.cantidadDeImpares();			 
+    	assertEquals(cantidadDeNumerosImpares, 3);
+    }
+    /**
+     * Verificar la cantidad de multiplos
+     */
+    @Test
+    public void testMultiplos() {
+    	int cantidadDeMultiplosDe2 = counter.cantidadDeMultiplosDe(2);
+    	int cantidadDeMultiplosDe5 = counter.cantidadDeMultiplosDe(5);
+    	assertEquals(cantidadDeMultiplosDe2, 2);
+    	assertEquals(cantidadDeMultiplosDe5, 3);
+    }
+}
